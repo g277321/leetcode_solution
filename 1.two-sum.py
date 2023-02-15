ca73c855cf9d2e54368f67d3482ae467166b5998
PPG <g277321@gmail.com>
2023-02-15 17:07:17 +0100

    test leetcode

---
+++ 1.two-sum.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
+# @lc code=end
